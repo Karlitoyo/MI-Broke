850df3f6f0e30348ee536ccaa960ba073043bf68
felixlionkeller <58859394+felixlionkeller@users.noreply.github.com>
2020-04-25 12:44:11 +0200

    Add files via upload

--- back_end_function.R
+++ back_end_function.R
@@ -1,11 +1,20 @@
+rm(list = objects())
 # input_functions ------------------------------------------------------------------------------
 
 # l0: Create list structure to guarantee that all elements exist
-l0 <- function(npar = 13, nass = 3, nlia = 3){
+namesass <- c("revenue", "cash", "equityinass")
+nameslia <- c("loanrep", "rent", "wages")
+
+vnames <- list(ass = namesass, lia = nameslia)
+
+l0 <- function(npar = 5, names = vnames){
   chars <- list(country = NA, industry = NA) # list of characters
-  pars <- data.frame(t(rep(1,npar)))
-  assets <-  data.frame(t(rep(NA,nass)))
-  liabilities <- data.frame(t(rep(NA,nlia)))
+  pars <- data.frame(t(c(rep(1,npar-1), 0)))
+  names(pars)[npar] <- "rf"
+  assets <-  data.frame(t(rep(NA,length(names$ass))))
+  names(assets) <- names$ass
+  liabilities <- data.frame(t(rep(NA,length(names$lia))))
+  names(liabilities) <- names$lia
   nums <- list(assets = assets, liabilities = liabilities, pars = pars ) # list of numerics
   l0  <- list(char = chars, num = nums) # dummy list
 }
@@ -17,15 +26,9 @@ l <- l0()
 
 l$char$country <- "Germany"
 l$char$industry <- "dog groomer"
-
-# l$num$assets$X1 <- 100
-# l$num$assets$X2<- 100
-# l$num$assets$X3<- 100
-# l$num$liabilities$X1 <- 20
-# l$num$liabilities$X2 <- 20
-# l$num$liabilities$X3 <- 20
 l$num$assets[1,] <- c(100, 100, 100)
 l$num$liabilities[1,] <- c(20,20,20)
+#l$num$pars$rf <- 0.5
 l
 
 # lc: function for sanity check 
@@ -56,11 +59,11 @@ tl <- function(st, g = 24, y = 12){
 survive <- function(l){
   if(lc(l)){
     st <- with(l$num, 
-               sum(assets)/sum(liabilities) # basic model 
+               (1-l$pars$rf^2)*sum(assets)/sum(liabilities) # basic model 
     )
     st <- max(c(0, st)) # if negative -> 0, should not be needed ... 
     cl <- tl(st) # traffic light color
-    rt <- list(st = st, cl = cl) # return list
+    rt <- list(st = st , cl = cl) # return list
     return(rt)
   }
   else{return(NA)} # return NA if the list is not well behaved
