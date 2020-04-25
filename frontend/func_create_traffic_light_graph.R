761a36531c82ee4e50e9956c41264590d55efedd
Skeir0n <64226680+Skeir0n@users.noreply.github.com>
2020-04-25 10:33:34 +0200

    Update func_create_traffic_light_graph.R

--- frontend/func_create_traffic_light_graph.R
+++ frontend/func_create_traffic_light_graph.R
@@ -1,77 +1,46 @@
 ### traffic light
-rm(list = ls())
-library(tidyverse)
-library(plotly)
 
-### define colors
+for(i in c("tidyverse", "plotly")){
+  if(i %in% rownames(installed.packages())){
+    library(i, character.only = T)
+  }else{
+    install.packages(i)
+    library(i, character.only = T)
+  }
+}
 
-
-### aux data.frame
-temp_df <- data.frame(constant = 1)
 ## create pie chart
 create_traffic_light <- function(indicator = "red", ...){
   
-  colors = c("red" = "rgb(255,0,0)", "orange" = "rgb(255,255,0)", "green" = "rgb(50,205,50)", "grey" = "rgb(105,105,105)")
+  colors = c("red" = "rgb(255,0,0)", "orange" = "rgb(255,69,0)", "green" = "rgb(50,205,50)", "grey" = "rgb(105,105,105)")
   
   temp_df <- data.frame(constant = 1)
   
-  p_red <- plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = 0),
-                   textposition = "none",
-                   hoverinfo  = "none",
-                   marker = list(colors = colors["red"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
-    layout(showlegend = F,
-           grid=list(rows=3, columns=1),
-           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  temp_color <- data.frame(colors = c("red", "orange", "green"),
+                           row_num = c(0, 1, 2))
   
-  p_orange <- plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = 1),
-                      textposition = "none",
-                      hoverinfo  = "none",
-                      marker = list(colors = colors["orange"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
-    layout(showlegend = F,
-           grid=list(rows=3, columns=1),
-           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  for(i in 1:nrow(temp_color)){
+    assign(paste0("p_", temp_color[i,1]), plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = temp_color[i, 2]),
+                                                 textposition = "none",
+                                                 hoverinfo  = "none",
+                                                 marker = list(colors = colors[i],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
+             layout(showlegend = F,
+                    grid=list(rows=3, columns=1),
+                    xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                    yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)) )
+  }
   
-  
-  p_green <- plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = 2),
-                     textposition = "none",
-                     hoverinfo  = "none",
-                     marker = list(colors = colors["green"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
-    layout(showlegend = F,
-           grid=list(rows=3, columns=1),
-           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-  
-  p_grey_0 <- plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = 0),
-                     textposition = "none",
-                     hoverinfo  = "none",
-                     marker = list(colors = colors["grey"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
-    layout(showlegend = F,
-           grid=list(rows=3, columns=1),
-           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-  
-  p_grey_1 <- plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = 1),
-                   textposition = "none",
-                   hoverinfo  = "none",
-                   marker = list(colors = colors["grey"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
-    layout(showlegend = F,
-           grid=list(rows=3, columns=1),
-           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-  
-  
-  p_grey_2 <- plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = 2),
-                     textposition = "none",
-                     hoverinfo  = "none",
-                     marker = list(colors = colors["grey"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
-    layout(showlegend = F,
-           grid=list(rows=3, columns=1),
-           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-  
-  
+  for(j in 0:2){
+    assign(paste0("p_grey_", j), plot_ly(data = temp_df, type ="pie", values = ~constant, domain = list(row = j),
+                        textposition = "none",
+                        hoverinfo  = "none",
+                        marker = list(colors = colors["grey"],line = list(color = 'rgb(0,0,0)', width = 2)))%>%
+      layout(showlegend = F,
+             grid=list(rows=3, columns=1),
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)))
+    
+  }  
   
   if(indicator %in% "red"){
     p_temp <- subplot(p_red, p_grey_1, p_grey_2, nrows = 3)
@@ -92,4 +61,3 @@ create_traffic_light <- function(indicator = "red", ...){
   
   p_temp
 }
-
